aa59371197883a062ae0d73d06abe2975d613084
Pablo Gerez <pgerez@gmail.com>
2025-02-09 00:36:16 -0300

    primer commit 	nuevos archivos: .env 	nuevos archivos: .env.dev 	nuevos archivos: .env.test 	nuevos archivos: .gitignore 	nuevos archivos: .idea/bustickets.iml 	nuevos archivos: .idea/codeception.xml 	nuevos archivos: .idea/commandlinetools/Symfony_26_12_24_09_53.xml 	nuevos archivos: .idea/commandlinetools/schemas/frameworkDescriptionVersion1.1.4.xsd 	nuevos archivos: .idea/modules.xml 	nuevos archivos: .idea/php.xml 	nuevos archivos: .idea/phpspec.xml 	nuevos archivos: .idea/phpunit.xml 	nuevos archivos: .idea/sumapp.iml 	nuevos archivos: .idea/workspace.xml 	nuevos archivos: README.md 	nuevos archivos: assets/app.js 	nuevos archivos: assets/bootstrap.js 	nuevos archivos: assets/controllers.json 	nuevos archivos: assets/controllers/hello_controller.js 	nuevos archivos: assets/styles/app.css 	nuevos archivos: bin/console 	nuevos archivos: bin/phpunit 	nuevos archivos: compose.override.yaml 	nuevos archivos: compose.yaml 	nuevos archivos: composer.json 	nuevos archivos: composer.lock 	nuevos archivos: config/bundles.php 	nuevos archivos: config/packages/asset_mapper.yaml 	nuevos archivos: config/packages/cache.yaml 	nuevos archivos: config/packages/debug.yaml 	nuevos archivos: config/packages/doctrine.yaml 	nuevos archivos: config/packages/doctrine_migrations.yaml 	nuevos archivos: config/packages/framework.yaml 	nuevos archivos: config/packages/mailer.yaml 	nuevos archivos: config/packages/messenger.yaml 	nuevos archivos: config/packages/monolog.yaml 	nuevos archivos: config/packages/notifier.yaml 	nuevos archivos: config/packages/routing.yaml 	nuevos archivos: config/packages/security.yaml 	nuevos archivos: config/packages/sonata_admin.yaml 	nuevos archivos: config/packages/sonata_user.yaml 	nuevos archivos: config/packages/translation.yaml 	nuevos archivos: config/packages/twig.yaml 	nuevos archivos: config/packages/validator.yaml 	nuevos archivos: config/packages/web_profiler.yaml 	nuevos archivos: config/preload.php 	nuevos archivos: config/routes.yaml 	nuevos archivos: config/routes/framework.yaml 	nuevos archivos: config/routes/security.yaml 	nuevos archivos: config/routes/sonata_admin.yaml 	nuevos archivos: config/routes/web_profiler.yaml 	nuevos archivos: config/services.yaml 	nuevos archivos: importmap.php 	nuevos archivos: migrations/.gitignore 	nuevos archivos: phpunit.xml.dist 	nuevos archivos: public/index.php 	nuevos archivos: src/Controller/.gitignore 	nuevos archivos: src/Entity/.gitignore 	nuevos archivos: src/Entity/User.php 	nuevos archivos: src/Kernel.php 	nuevos archivos: src/Repository/.gitignore 	nuevos archivos: src/Repository/UserRepository.php 	nuevos archivos: symfony.lock 	nuevos archivos: templates/base.html.twig 	nuevos archivos: tests/bootstrap.php 	nuevos archivos: translations/.gitignore

---
+++ src/Entity/User.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Entity;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Sonata\UserBundle\Entity\BaseUser;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: 'fos_user_user')]
+class User extends BaseUser
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    protected $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    
+}
