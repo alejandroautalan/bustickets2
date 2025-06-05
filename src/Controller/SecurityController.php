02a2575ef858a0b07ad273d6dde490422f889da0
Alejandro Autalan <alejandroautalan@gmail.com>
2025-06-05 00:43:36 -0300

    Agrego registro rapido de usuario. Falta completar.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,6 +16,11 @@ use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Notifier\Recipient\Recipient;
 use Symfony\Component\Security\Http\LoginLink\LoginLinkNotification;
 
+use Sonata\UserBundle\Model\UserManagerInterface;
+
+use App\Form\Model\Registro;
+use App\Form\Type\RegistroType;
+
 
 class SecurityController extends AbstractController
 {
@@ -27,6 +32,10 @@ class SecurityController extends AbstractController
             // load the user in some way (e.g. using the form input)
             $email = $request->getPayload()->get('email');
             $user = $userRepository->findOneBy(['email' => $email]);
+
+            if(null == $user) {
+                return $this->redirectToRoute('register', ['email' => $email]);
+            }
 
             // create a login link for $user this returns an instance
             // of LoginLinkDetails
@@ -50,6 +59,46 @@ class SecurityController extends AbstractController
 
         // if it's not submitted, render the form to request the "login link"
         return $this->render('security/request_login_link.html.twig');
+    }
+
+    #[Route('/register', name: 'register')]
+    public function registerAndSendLoginLink(
+        NotifierInterface $notifier,
+        LoginLinkHandlerInterface $loginLinkHandler,
+        UserRepository $userRepository,
+        Request $request,
+        UserManagerInterface $userManager
+    ): Response
+    {
+        $email = $request->get('email');
+        $registro = new Registro();
+        $registro->setEmail($email);
+        $form = $this->createForm(RegistroType::class, $registro);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $registro = $form->getData();
+            $username = $registro->getEmail();
+            $email = $registro->getEmail();
+            $password = bin2hex(random_bytes(64 / 2));  # 32 caracteres
+
+            $user = $userManager->create();
+            $user->setUsername($username);
+            $user->setEmail($email);
+            $user->setPlainPassword($password);
+            $user->setEnabled(true);
+            $user->setSuperAdmin(false);
+
+            $userManager->save($user);
+
+            return $this->redirectToRoute('task_success');
+        }
+
+        $context = [
+            'form' => $form,
+        ];
+
+        return $this->render('security/register_withlink.html.twig', $context);
     }
 
     #[Route('/login_check', name: 'login_check')]
